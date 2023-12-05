df76dd93f5d40b14912e6433b831f52457016a7c
bengagioussama <oussama.bengagi@esprit.tn>
2023-12-05 10:11:01 +0100

    added service interface

---
+++ src/Services/IService.java
@@ -0,0 +1,12 @@
+package Services;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface IService <T>{
+    void add(T t) throws SQLException;
+    void update(T t) throws  SQLException;
+    void delete(int id) throws  SQLException;
+    ArrayList<T> readAll() throws  SQLException;
+    T get(int id) throws SQLException;
+}
