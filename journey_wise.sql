285512948acb10ada2473cbb98d5e19838edf3eb
Hamza Jomni <99356035+HamzaJomni@users.noreply.github.com>
2023-12-12 11:13:30 +0100

    final database

---
+++ journey_wise.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 12 déc. 2023 à 11:11
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `journey_wise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `journal`
+--
+
+CREATE TABLE `journal` (
+  `id` int(11) NOT NULL,
+  `offreVoyage` varchar(100) NOT NULL,
+  `paysVoyager` varchar(50) NOT NULL,
+  `titre` varchar(100) NOT NULL,
+  `descrption` varchar(500) NOT NULL,
+  `url_image` varchar(500) NOT NULL,
+  `id_user` int(100) NOT NULL,
+  `id_res` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres`
+--
+
+CREATE TABLE `offres` (
+  `id_offre` int(11) NOT NULL,
+  `pays` varchar(20) NOT NULL,
+  `cite` varchar(20) NOT NULL,
+  `lieu` varchar(30) NOT NULL,
+  `date` varchar(15) NOT NULL,
+  `prix` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `offres`
+--
+
+INSERT INTO `offres` (`id_offre`, `pays`, `cite`, `lieu`, `date`, `prix`) VALUES
+(1, 'Turkey', 'Istambul', 'xxxx', '10-09-2023', 1852.8);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reclamation`
+--
+
+CREATE TABLE `reclamation` (
+  `id` int(11) NOT NULL,
+  `dateReclamation` date NOT NULL,
+  `objet` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `dateDebut` date NOT NULL,
+  `dateFin` date NOT NULL,
+  `nbrPass` int(11) NOT NULL,
+  `idOffre` int(11) NOT NULL,
+  `idMembre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `journal`
+--
+ALTER TABLE `journal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_res` (`id_res`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD PRIMARY KEY (`id_offre`);
+
+--
+-- Index pour la table `reclamation`
+--
+ALTER TABLE `reclamation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idOffre` (`idOffre`),
+  ADD KEY `idMembre` (`idMembre`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `journal`
+--
+ALTER TABLE `journal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `offres`
+--
+ALTER TABLE `offres`
+  MODIFY `id_offre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `reclamation`
+--
+ALTER TABLE `reclamation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `journal`
+--
+ALTER TABLE `journal`
+  ADD CONSTRAINT `journal_ibfk_1` FOREIGN KEY (`id_res`) REFERENCES `reservations` (`id`),
+  ADD CONSTRAINT `journal_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`idOffre`) REFERENCES `offres` (`id_offre`),
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`idMembre`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
